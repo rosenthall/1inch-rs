48480216b4f3bb8e8a3d4775528dd01ab191b36e
Rosenthall <rosenthall@riseup.net>
2023-08-01 09:43:51 +0300

    init

---
+++ target/debug/deps/OneInch-a585b0761bec68cb.d
@@ -0,0 +1,7 @@
+/usr/home/armagg/CLionProjects/1inch-rs/OneInch/target/debug/deps/OneInch-a585b0761bec68cb.rmeta: src/lib.rs
+
+/usr/home/armagg/CLionProjects/1inch-rs/OneInch/target/debug/deps/libOneInch-a585b0761bec68cb.rlib: src/lib.rs
+
+/usr/home/armagg/CLionProjects/1inch-rs/OneInch/target/debug/deps/OneInch-a585b0761bec68cb.d: src/lib.rs
+
+src/lib.rs:
---
+++ target/debug/libOneInch.d
@@ -0,0 +1,1 @@
+/usr/home/armagg/CLionProjects/1inch-rs/OneInch/target/debug/libOneInch.rlib: /usr/home/armagg/CLionProjects/1inch-rs/OneInch/src/lib.rs
