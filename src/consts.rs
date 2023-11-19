e77d80411ce6fa9b50415bdd8863dbeabbc55b7e
armagg <rosenthall@riseup.net>
2023-11-19 03:39:22 +0200

    Hardcoded versions of api in separate file

---
+++ src/consts.rs
@@ -0,0 +1,17 @@
+#![allow(unused_variables)]
+#![allow(dead_code)]
+
+
+pub const BASIC_URL : &str = "https://api.1inch.dev";
+
+pub const SWAP_API_VERSION : &str = "v5.2";
+pub const ORDERBOOK_API_VERSION : &str = "v3.0";
+pub const FUSION_API_VERSION : &str = "v1.0";
+pub const HISTORY_API_VERSION : &str = "v2.0";
+pub const TRACES_API_VERSION : &str = "v1.0";
+pub const PORTFOLIO_API_VERSION : &str = "v3";
+pub const BALANCE_API_VERSION : &str = "v1.2";
+pub const GAS_PRICE_API_VERSION : &str = "v1.4";
+pub const SPOT_PRICE_API_VERSION : &str = "v1.1";
+pub const TOKENS_API_VERSION : &str = "v1.2";
+pub const NFT_API_VERSION : &str = "v1";
