e885cc015dcf9600f091103f80ab5dbb4397ce2d
armagg <rosenthall@riseup.net>
2023-11-25 22:30:54 +0200

    Refactored src/swap and make it work.

--- src/swap/swap.rs
+++ src/swap/swap.rs
@@ -1,1 +1,103 @@
+use std::error::Error;
+use std::io::ErrorKind;
+use reqwest::Url;
+use crate::client::OneInchClient;
+use crate::consts::{BASIC_URL, SWAP_API_VERSION};
+use crate::swap::{SwapDetails, SwapError, SwapRequestError, SwapResponse};
+use crate::utils::params::insert_optional_param;
 
+impl OneInchClient {
+    /// Gets a `SwapTrancationData`.
+    pub async fn swap(
+        &self,
+        details: SwapDetails,
+    ) -> Result<SwapResponse, Box<dyn Error>> {
+        let url = format!(
+            "{}/swap/{}/{}/swap/",
+            BASIC_URL,
+            SWAP_API_VERSION,
+            self.network_id
+        );
+
+        // Adding required parameters
+        let mut params: Vec<(&str, String)> = vec![
+            ("from", details.from),
+            ("slippage", details.slippage.to_string()),
+            ("src", details.src),
+            ("dst", details.dst),
+            ("amount", details.amount),
+        ];
+
+
+        // Adding optional bool parameters
+        insert_optional_param(&mut params, "disableEstimate", details.disable_estimate.map(|a| a.to_string()));
+        insert_optional_param(&mut params, "allowPartialFill", details.allow_partial_fill.map(|a| a.to_string()));
+        insert_optional_param(&mut params, "includeGas", details.include_gas.map(|a| a.to_string()));
+        insert_optional_param(&mut params, "includeProtocols", details.include_protocols.map(|a| a.to_string()));
+        insert_optional_param(&mut params, "includeTokensInfo", details.include_tokens_info.map(|a| a.to_string()));
+
+
+        // Adding optional num parameters
+        insert_optional_param(&mut params, "fee", details.fee.map(|a| a.to_string()));
+        insert_optional_param(&mut params, "complexityLevel", details.complexity_level.map(|a| a.to_string()));
+        insert_optional_param(&mut params, "parts", details.parts.map(|a| a.to_string()));
+        insert_optional_param(&mut params, "mainRouteParts", details.main_route_parts.map(|a| a.to_string()));
+        insert_optional_param(&mut params, "gasLimit", details.gas_limit.map(|a| a.to_string()));
+
+        // Adding optional string parameters
+        insert_optional_param(&mut params, "protocols", details.protocols);
+        insert_optional_param(&mut params, "gasPrice", details.gas_price);
+        insert_optional_param(&mut params, "connectorTokens", details.connector_tokens);
+        insert_optional_param(&mut params, "permit", details.permit);
+        insert_optional_param(&mut params, "receiver", details.receiver);
+        insert_optional_param(&mut params, "referrer", details.referrer);
+
+
+
+
+        let url_with_params =
+            Url::parse_with_params(&url, params).map_err(|e| Box::new(e) as Box<dyn Error>)?;
+
+        dbg!(&url_with_params);
+
+        let response = match self
+            .http_client
+            .get(url_with_params)
+            .header("Authorization", &self.token)
+            .send()
+            .await {
+            Ok(response) => response,
+            Err(e) => return Err(SwapError::Network(e).into()),
+        };
+
+        if response.status().as_u16() == 400 {
+            let error_body = response.text().await.unwrap_or_default();
+            match serde_json::from_str::<SwapRequestError>(&error_body) {
+                Ok(err) => {
+                    return Err(SwapError::SwapRequest {
+                        description: err.description,
+                        error: err.error,
+                        status_code: err.status_code,
+                        request_id: err.request_id,
+                    }.into())
+                },
+                Err(e) => {
+                    // Преобразование reqwest::Error в serde_json::Error не является тривиальным,
+                    // поэтому используем SwapError::Other для непредвиденных ошибок
+                    return Err(SwapError::Other(format!("Error parsing error response: {}", e)).into())
+                },
+            }
+        }
+
+        if response.status().is_client_error() || response.status().is_server_error() {
+            return Err(SwapError::Other(format!("Server responded with error: {}", response.status())).into());
+        }
+
+        let swap_data: SwapResponse = match response.json().await {
+            Ok(data) => data,
+            Err(e) => return Err(SwapError::Network(e).into()),
+        };
+
+        Ok(swap_data)
+    }
+}
--- src/swap/types.rs
+++ src/swap/types.rs
@@ -2,6 +2,7 @@ use crate::builder_setter;
 use num_bigint::BigUint;
 use serde::{Deserialize, Serialize};
 use thiserror::Error;
+use crate::common::token::TokenInfo;
 
 /// Enumerates potential errors when constructing `SwapDetails`.
 #[derive(Error, Debug, Eq, PartialEq)]
@@ -13,57 +14,249 @@ pub enum SwapDetailsBuilderError {
     /// Indicates the provided slippage value is outside the allowable range.
     #[error("Invalid slippage value. It should be between 0 and 50.")]
     InvalidSlippage,
+
+    #[error("Invalid fee value. It should be between 0 and 3.")]
+    InvalidFee
 }
 
 /// Represents the details required for performing a token swap.
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug)]
 pub struct SwapDetails {
-    src: String,              // Source token address.
-    dst: String,              // Destination token address.
-    amount: String,           // Amount to be swapped.
-    from: String,             // Address of the user initiating the swap.
-    slippage: usize,          // Permitted slippage percentage.
-    disable_estimate: bool,   // If true, disables estimation.
-    allow_partial_fill: bool, // If true, allows the swap to be partially filled.
+    pub src: String,              // Source token address.
+    pub dst: String,              // Destination token address.
+    pub amount: String,           // Amount to be swapped.
+    pub from: String,             // Address of the user initiating the swap.
+    pub slippage: usize,          // Permitted slippage percentage.
+
+    // Optional fields
+    pub fee : Option<u8>,
+    pub protocols : Option<String>,
+    pub gas_price : Option<String>,
+    pub complexity_level : Option<u128>,
+    pub parts : Option<u128>,
+    pub main_route_parts : Option<u128>,
+    pub gas_limit : Option<u128>,
+
+    pub include_tokens_info : Option<bool>,
+    pub include_protocols : Option<bool>,
+    pub include_gas : Option<bool>,
+    pub connector_tokens : Option<String>,
+    pub permit : Option<String>,
+    pub receiver : Option<String>,
+    pub referrer : Option<String>,
+
+    pub disable_estimate: Option<bool>,   // If true, disables estimation.
+    pub allow_partial_fill: Option<bool>, // If true, allows the swap to be partially filled.
 }
 
 /// A builder pattern implementation for creating a `SwapDetails`.
 pub struct SwapDetailsBuilder {
-    from_token_addr: Option<String>,
-    to_token_addr: Option<String>,
-    amount: Option<BigUint>,
+    src: Option<String>,
+    dst: Option<String>,
+    amount: Option<String>,
     from_addr: Option<String>,
     slippage: Option<usize>,
-    disable_estimate: Option<bool>,
-    allow_partial_fill: Option<bool>,
+
+    // Optional fields
+    fee : Option<u8>,
+    protocols : Option<String>,
+    gas_price : Option<String>,
+    complexity_level : Option<u128>,
+    parts : Option<u128>,
+    main_route_parts : Option<u128>,
+    gas_limit : Option<u128>,
+
+    include_tokens_info : Option<bool>,
+    include_protocols : Option<bool>,
+    include_gas : Option<bool>,
+    connector_tokens : Option<String>,
+    permit : Option<String>,
+    receiver : Option<String>,
+    referrer : Option<String>,
+
+    disable_estimate: Option<bool>,   // If true, disables estimation.
+    allow_partial_fill: Option<bool>, // If true, allows the swap to be partially filled.
 }
 
-impl Default for SwapDetailsBuilder {
-    fn default() -> Self {
-        Self::new()
-    }
+#[derive(Deserialize, Debug)]
+pub struct SwapResponse {
+    #[serde(rename = "fromToken")]
+    pub from_token: TokenInfo,
+
+    #[serde(rename = "toToken")]
+    pub to_token: TokenInfo,
+
+    #[serde(rename = "toAmount")]
+    pub to_amount: String,
+
+    pub protocols: Vec<Vec<Vec<SelectedProtocol>>>,
+
+
+    #[serde(rename = "tx")]
+    pub transaction : SwapTranactionData
 }
+
+
+#[derive(Deserialize,Debug)]
+pub struct SwapTranactionData {
+    from : String,
+    to : String,
+    data : String,
+    value : String,
+
+    #[serde(rename = "gasPrice")]
+    gas_price : String,
+
+
+    gas : u128
+}
+
+
+
+/// Represents errors that can occur during swap operations.
+///
+/// This enum aggregates various types of errors related to swap operations,
+/// including HTTP requests, JSON parsing, and swap API specific errors.
+#[derive(Error, Debug)]
+pub enum SwapError {
+    /// Error related to network requests.
+    ///
+    /// Used for handling issues with network requests, such as server
+    /// unavailability, network connectivity problems, etc.
+    #[error("Network error: {0}")]
+    Network(reqwest::Error),
+
+    /// Error while parsing JSON.
+    ///
+    /// Occurs when the server's response cannot be correctly deserialized from JSON.
+    /// This could happen if the response format is different than expected.
+    #[error("JSON parsing error: {0}")]
+    JsonParse(serde_json::Error),
+
+    /// Specific error related to swap API.
+    ///
+    /// Represents errors specific to the swap API, like insufficient funds or invalid
+    /// request parameters.
+    #[error("Swap request error: {description}")]
+    SwapRequest {
+        description: String,
+        error: String,
+        status_code: u16,
+        request_id: String,
+    },
+
+    /// A general error.
+    ///
+    /// Used for other types of errors that do not fit into the above categories.
+    #[error("Other error: {0}")]
+    Other(String),
+}
+
+
+/// Represents an error response from the swap API.
+///
+/// This structure is used to deserialize the JSON error response from the swap API.
+/// It contains details about the error that occurred during a swap request.
+#[derive(serde::Deserialize)]
+pub struct SwapRequestError {
+    /// A brief description of the error.
+    pub error: String,
+
+    /// A more detailed description of the error.
+    pub description: String,
+
+    /// The HTTP status code associated with the error.
+    #[serde(rename = "statusCode")]
+    pub status_code: u16,
+
+    /// A unique identifier for the request, useful for debugging.
+    #[serde(rename = "requestId")]
+    pub request_id: String,
+
+    /// Additional metadata related to the error, if any.
+    pub meta: Vec<HttpExceptionMeta>,
+}
+
+
+/// Represents additional metadata in the swap API error response.
+///
+/// Each item in the `meta` field of `SwapRequestError` will be deserialized into this structure.
+/// It provides more context about the error, such as the affected parameters or values.
+#[derive(serde::Deserialize)]
+pub struct HttpExceptionMeta {
+    /// The type of metadata.
+    #[serde(rename = "type")]
+    type_field: String,
+
+    /// The value associated with this metadata.
+    value: String,
+}
+
 
 impl SwapDetailsBuilder {
     /// Constructs a new `SwapDetailsBuilder` with all fields uninitialized.
     pub fn new() -> Self {
         SwapDetailsBuilder {
-            from_token_addr: None,
-            to_token_addr: None,
+            src: None,
+            dst: None,
             amount: None,
             from_addr: None,
             slippage: None,
+
+            fee: None,
+            protocols: None,
+            gas_price: None,
+            complexity_level: None,
+            parts: None,
+            main_route_parts: None,
+            gas_limit: None,
+            include_tokens_info: None,
+            include_protocols: None,
+            include_gas: None,
+            connector_tokens: None,
+            permit: None,
+            receiver: None,
+            referrer: None,
             disable_estimate: None,
             allow_partial_fill: None,
         }
     }
 
-    builder_setter!(from_token_addr, String);
-    builder_setter!(to_token_addr, String);
-    builder_setter!(amount, BigUint);
+    builder_setter!(src, String);
+    builder_setter!(dst, String);
+    builder_setter!(amount, String);
     builder_setter!(from_addr, String);
+
+    builder_setter!(protocols, String);
+    builder_setter!(gas_price, String);
+    builder_setter!(complexity_level, u128);
+    builder_setter!(parts, u128);
+    builder_setter!(main_route_parts, u128);
+    builder_setter!(gas_limit, u128);
+
+    builder_setter!(include_tokens_info, bool);
+    builder_setter!(include_protocols, bool);
+    builder_setter!(include_gas, bool);
+
+
+    builder_setter!(connector_tokens, String);
+    builder_setter!(permit, String);
+    builder_setter!(receiver, String);
+    builder_setter!(referrer, String);
+
     builder_setter!(disable_estimate, bool);
     builder_setter!(allow_partial_fill, bool);
+
+    /// Special setter for fee that ensures value is within allowable range.
+    pub fn fee(mut self, fee: u8) -> Result<Self, SwapDetailsBuilderError> {
+        if fee > 3 {
+            return Err(SwapDetailsBuilderError::InvalidFee);
+        }
+        self.fee = Some(fee);
+        Ok(self)
+    }
+
+
 
     /// Special setter for slippage that ensures value is within allowable range.
     pub fn slippage(mut self, slippage: usize) -> Result<Self, SwapDetailsBuilderError> {
@@ -78,11 +271,11 @@ impl SwapDetailsBuilder {
     pub fn build(self) -> Result<SwapDetails, SwapDetailsBuilderError> {
         Ok(SwapDetails {
             src: self
-                .from_token_addr
-                .ok_or(SwapDetailsBuilderError::MissingField("from_token_addr"))?,
+                .src
+                .ok_or(SwapDetailsBuilderError::MissingField("src"))?,
             dst: self
-                .to_token_addr
-                .ok_or(SwapDetailsBuilderError::MissingField("to_token_addr"))?,
+                .dst
+                .ok_or(SwapDetailsBuilderError::MissingField("dst"))?,
             amount: self
                 .amount
                 .ok_or(SwapDetailsBuilderError::MissingField("amount"))?
@@ -93,11 +286,42 @@ impl SwapDetailsBuilder {
             slippage: self
                 .slippage
                 .ok_or(SwapDetailsBuilderError::MissingField("slippage"))?,
-            disable_estimate: self.disable_estimate.unwrap_or(false),
-            allow_partial_fill: self.allow_partial_fill.unwrap_or(false),
+
+
+            fee: self.fee,
+            protocols: self.protocols,
+            gas_price: self.gas_price,
+            complexity_level: self.complexity_level,
+            parts: self.parts,
+            main_route_parts: self.main_route_parts,
+            gas_limit: self.gas_limit,
+            include_tokens_info: self.include_tokens_info,
+            include_protocols: self.include_protocols,
+            include_gas: self.include_gas,
+            connector_tokens: self.connector_tokens,
+            permit: self.permit,
+            receiver: self.receiver,
+            referrer: self.referrer,
+            disable_estimate: self.disable_estimate,
+            allow_partial_fill: self.allow_partial_fill,
         })
     }
 }
+
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct SelectedProtocol {
+    pub name: String,
+    pub part: f64,
+
+    #[serde(rename = "fromTokenAddress")]
+    pub from_token_address: String,
+
+    #[serde(rename = "toTokenAddress")]
+    pub to_token_address: String,
+}
+
+
 
 /// Tests for the `SwapDetailsBuilder` and related components.
 #[cfg(test)]
@@ -108,9 +332,9 @@ mod tests {
     #[test]
     fn test_valid_swap_details_builder() {
         let swap_details = SwapDetailsBuilder::new()
-            .from_token_addr("from_token".to_string())
-            .to_token_addr("to_token".to_string())
-            .amount(BigUint::from(1000u32))
+            .src("from_token".to_string())
+            .dst("to_token".to_string())
+            .amount("1000".to_string())
             .from_addr("from_addr".to_string())
             .slippage(5)
             .expect("Invalid slippage")
@@ -132,9 +356,9 @@ mod tests {
     #[test]
     fn test_invalid_slippage_in_builder() {
         let result = SwapDetailsBuilder::new()
-            .from_token_addr("from_token".to_string())
-            .to_token_addr("to_token".to_string())
-            .amount(BigUint::from(1000u32))
+            .src("from_token".to_string())
+            .dst("to_token".to_string())
+            .amount("1000".to_string())
             .from_addr("from_addr".to_string())
             .slippage(102);
 
