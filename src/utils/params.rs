e4baf17084d206e4199ec50c93ccfbfbe2874d0e
armagg <rosenthall@riseup.net>
2023-11-29 19:41:58 +0200

    Added test for insert_optional_param

--- src/utils/params.rs
+++ src/utils/params.rs
@@ -5,3 +5,22 @@ pub(crate) fn insert_optional_param<'a>(params: &mut Vec<(&'a str, String)>, par
         params.push((param_name, v));
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_insert_optional_param_with_both_cases() {
+        let mut params = vec![("existing_param", "existing_value".to_string())];
+
+        // Test that no field is added when the value is None
+        insert_optional_param(&mut params, "none_param", None);
+        assert_eq!(params.len(), 1); // No field should be added
+
+        // Test that a field is added when the value is Some
+        insert_optional_param(&mut params, "new_param", Some("new_value".to_string()));
+        assert_eq!(params.len(), 2); // A new field should be added
+        assert_eq!(params[1], ("new_param", "new_value".to_string())); // Check the added field
+    }
+}
